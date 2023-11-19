97b073804da99f96c49b59833541905970e2268a
Noufal N <noufaln26@gmail.com>
2023-11-19 15:43:00 +0530

    Initial commit with Phaser Setup

---
+++ js/game.js
@@ -0,0 +1,31 @@
+const config = {
+    type: Phaser.AUTO,
+    width: 800,
+    height: 600,
+    physics: {
+        default: 'arcade',
+        arcade: {
+            gravity: { y: 300 },
+            debug: false
+        }
+    },
+    scene: {
+        preload: preload,
+        create: create,
+        update: update
+    }
+};
+
+var game = new Phaser.Game(config);
+
+function preload() {
+    // Load images and sounds
+}
+
+function create() {
+    // Create game entities and set up initial game state
+}
+
+function update() {
+    // Game loop code goes here
+}
